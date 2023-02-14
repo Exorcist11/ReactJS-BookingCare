2207e438d847c86871c186c44adc244362d0924d
Exorcist <dungnguyenx2077@gmail.com>
2023-02-15 00:53:32 +0700

    #47 change language

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -3,11 +3,15 @@ import { connect } from 'react-redux';
 import './HomeHeader.scss';
 import logo from '../../assets/images/logo.svg';
 import { FormattedMessage } from 'react-intl';
+import { LANGUAGES } from '../../utils/constant';
+import { changeLanguageApp } from '../../store/actions/appActions';
 
 class HomeHeader extends Component {
-
+    changeLanguage = (language) => {
+        this.props.changeLanguageAppRedux(language)
+    }
     render() {
-        console.log('Check props ', this.props);
+        let language = this.props.language;
         return (
             <React.Fragment>
                 <div className='home-header-container'>
@@ -43,8 +47,8 @@ class HomeHeader extends Component {
                             <div className='support'><i className="fas fa-question-circle"></i>
                                 <FormattedMessage id='homeheader.support' />
                             </div>
-                            <div className='language-vi'>VN</div>
-                            <div className='language-en'>EN</div>
+                            <div className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}><span onClick={() => this.changeLanguage(LANGUAGES.VI)}>VN</span></div>
+                            <div className={language === LANGUAGES.EN ? 'language-en active' : 'language-en'}><span onClick={() => this.changeLanguage(LANGUAGES.EN)}>EN</span></div>
                         </div>
                     </div>
                 </div>
@@ -62,7 +66,7 @@ class HomeHeader extends Component {
                         <div className='options'>
                             <div className='option-child'>
                                 <div className='icon-child'><i className="far fa-hospital"></i></div>
-                                <div className='text-child'><FormattedMessage id='banner.specialized-examination"' /></div>
+                                <div className='text-child'><FormattedMessage id='banner.specialized-examination' /></div>
                             </div>
                             <div className='option-child'>
                                 <div className='icon-child'><i className="fas fa-mobile-alt"></i></div>
@@ -102,6 +106,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
+        changeLanguageAppRedux: (language) => dispatch(changeLanguageApp(language))
     };
 };
 
