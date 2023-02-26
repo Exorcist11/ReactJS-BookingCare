3b6504907a52feecb8195f8cf9b6b0ebd9eb72de
Exorcist <dungnguyenx2077@gmail.com>
2023-02-26 19:36:10 +0700

    #50 handnook, medical facility, top doctor

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import './HomePage.scss';
 import HomeHeader from './HomeHeader';
 import Specialty from './Section/Specialty';
-
+import MedicalFacility from './Section/MedicalFacility';
+import OutStandingDoctor from './Section/OutStandingDoctor';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import HandBook from './Section/HandBook';
 class HomePage extends Component {
 
     render() {
-
+        let settings = {
+            dots: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 1
+        };
         return (
             <div>
                 <HomeHeader />
-                <Specialty />
+                <Specialty settings={settings} />
+                <MedicalFacility settings={settings} />
+                <OutStandingDoctor settings={settings} />
+                <HandBook settings={settings} />
             </div>
         );
     }
